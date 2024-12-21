ca14e0dbeca9e199296717edb499a9c0575589f6
Tal Zohar <tal.zohar3@gmail.com>
2024-12-21 16:53:47 +0200

    add result file

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from random import randint
 from itertools import chain
 
 LARGE = 10000000
-N = 5
-K = 2
+N = 6
+K = 3
 REQUEST_NUM = 10
-
+RESULT_FILE = 'result.txt'
 
 def max_index(my_list):
     return my_list.index(max(my_list))
@@ -20,7 +20,6 @@ def generate_graph_from_requests(requests, page_weights, k, required_cache_state
 
     required_cache_states = [current_required | {request} for current_required, request in
                              zip(required_cache_states, requests)]
-    print(f"required cache states: {required_cache_states}")
 
     G = nx.DiGraph()
     pos = 0
@@ -92,21 +91,66 @@ def solve_weighted_paging(requests, page_weights, k, required_cache_states=None)
     return cache_states, paging_cost
 
 
-def main():
-    page_weights = [2 ** i for i in range(N)]
-    requests = [randint(0, N - 1) for i in range(REQUEST_NUM)]
-    print(f"page_weight: {page_weights}")
-    print(f"requests: {requests}")
-    cs1_cache_states, cs1_paging_cost = solve_weighted_paging(requests, page_weights, K)
-    print(f"k OPT cache_states {cs1_cache_states}")
-    print(f"k OPT cost {cs1_paging_cost}")
-    cs2_cache_states, cs2_paging_cost = solve_weighted_paging(requests, page_weights, K + 1)
-    print(f"k+1 OPT cache_states {cs2_cache_states}")
-    print(f"k+1 OPT cost {cs2_paging_cost}")
-    new_cs2_cache_states, new_cs2_paging_cost = solve_weighted_paging(requests, page_weights, K + 1,
-                                                                      required_cache_states=cs1_cache_states)
-    print(f"k+1 cache_states {new_cs2_cache_states}")
-    print(f"k+1 cost {new_cs2_paging_cost}")
+def convert_cache_state_to_persistent_cache_state(requests, cache_states):
+    """
+    Converts cache state solution to contain only pages that remain in the cache between requests
+    """
+    new_cache_state = [set() for i in range(len(cache_states))]
+    for page in set(requests):
+        page_occurences = [i for i, x in enumerate(requests) if page == x]
+        page_occurences = [0, *page_occurences, len(requests) - 1]
+        for start, end in zip(page_occurences, page_occurences[1:]):
+            preserve = True
+            for state in cache_states[start:end + 1]:
+                if page not in state:
+                    preserve = False
 
+            if preserve:
+                for i in range(start, end + 1):
+                    new_cache_state[i].add(page)
+    return new_cache_state
+
+
+def main():
+    while True:
+        page_weights = [2 ** i for i in range(N)]
+        requests = [randint(0, N - 1) for i in range(REQUEST_NUM)]
+        print(f"page_weight: {page_weights}")
+        print(f"requests: {requests}")
+        cs1_cache_states, cs1_paging_cost = solve_weighted_paging(requests, page_weights, K)
+        print(f"k OPT cache_states {cs1_cache_states}")
+        print(f"k OPT cost {cs1_paging_cost}")
+        cs2_cache_states, cs2_paging_cost = solve_weighted_paging(requests, page_weights, K + 1)
+        print(f"k+1 OPT cache_states {cs2_cache_states}")
+        print(f"k+1 OPT cost {cs2_paging_cost}")
+        persistent_cs1_cache_state = convert_cache_state_to_persistent_cache_state(requests, cs1_cache_states)
+        print(f"persistent cache state {persistent_cs1_cache_state}")
+        new_cs2_cache_states, new_cs2_paging_cost = solve_weighted_paging(requests, page_weights, K + 1,
+                                                                          required_cache_states=persistent_cs1_cache_state)
+        print(f"k+1 cache_states {new_cs2_cache_states}")
+        print(f"k+1 cost {new_cs2_paging_cost}")
+        if cs2_paging_cost != new_cs2_paging_cost:
+            print(f"SUCCESS")
+            with open(RESULT_FILE, 'a') as file:
+                file.write("INCOMING RESULT\n")
+                file.write(f"page_weight: {page_weights}\n")
+                file.write(f"requests: {requests}\n")
+                file.write(f"k OPT cache_states {cs1_cache_states}\n")
+                file.write(f"k OPT cost {cs1_paging_cost}\n")
+                file.write(f"k+1 OPT cache_states {cs2_cache_states}\n")
+                file.write(f"k+1 OPT cost {cs2_paging_cost}\n")
+                file.write(f"persistent cache state {persistent_cs1_cache_state}\n")
+                file.write(f"k+1 cache_states {new_cs2_cache_states}\n")
+                file.write(f"k+1 cost {new_cs2_paging_cost}\n\n")
 if __name__ == '__main__':
+    requests = [0, 4, 2, 3, 2, 0, 3]
+    page_weights = [2 ** i for i in range(N)]
+    states, cost = (solve_weighted_paging(requests, page_weights, K))
+    states = convert_cache_state_to_persistent_cache_state(requests, states)
+    print(states, cost)
+    states, cost = (solve_weighted_paging(requests, page_weights, K + 1,
+                                          required_cache_states=states))
+    print(states, cost)
+    states, cost = (solve_weighted_paging(requests, page_weights, K + 1))
+    print(states, cost)
     main()
