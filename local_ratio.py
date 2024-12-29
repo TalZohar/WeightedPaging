04181284da91baa8afe825f346901c1ea7e50971
Tal Zohar <tal.zohar3@gmail.com>
2024-12-29 13:11:35 +0200

    fix local ration to behave like in paper

--- local_ratio.py
+++ local_ratio.py
@@ -15,12 +15,14 @@ class Interval:
     weight: float
     page: int
     request: int
-    is_starting_sol: bool
     id: uuid.UUID = dataclasses.field(default_factory=uuid.uuid4, init=False)
 
     @property
     def length(self):
         return self.end_time - self.start_time
+
+    def contains(self, t: int):
+        return self.start_time <= t <= self.end_time
 
 
 @dataclasses.dataclass
@@ -37,29 +39,26 @@ class IntervalProblem:
         for i in range(interval.start_time, interval.end_time + 1):
             self.interval_per_time[i].append(interval)
 
-    def find_starting_sol_intervals(self, time: int):
-        starting_sol_intervals = []
+    def find_weightless_intervals(self, time: int):
+        weightless_intervals = []
         for interval in self.interval_per_time[time]:
-            if interval.is_starting_sol:
-                starting_sol_intervals.append(interval)
-        return starting_sol_intervals
+            if interval.weight == 0:
+                weightless_intervals.append(interval)
+        return weightless_intervals
 
-    def find_non_starting_sol_intervals(self, time: int):
-        non_starting_sol_intervals = []
+    def find_non_weightless_intervals(self, time: int):
+        non_weightless_intervals = []
         for interval in self.interval_per_time[time]:
-            if not interval.is_starting_sol:
-                non_starting_sol_intervals.append(interval)
-        return non_starting_sol_intervals
+            if interval.weight > 0:
+                non_weightless_intervals.append(interval)
+        return non_weightless_intervals
 
     def find_tau(self):
         """
-        Tau is the smallest time step where the number of weightless intervals is less than width
-        :return: time, if not existent, return None
+        Tau is the time with the minimum excess
+        :return: time
         """
-        for t in range(self.time_horizon):
-            if len(self.find_starting_sol_intervals(t)) < self.width:
-                return t
-        return None
+        return min(range(self.time_horizon), key=lambda t: len(self.find_weightless_intervals(t)))
 
 
 def translate_paging_to_interval_problem(requests, page_weights, k, start_cache_state) -> IntervalProblem:
@@ -73,19 +72,17 @@ def translate_paging_to_interval_problem(requests, page_weights, k, start_cache_
     last_occurrences = [0] * len(page_weights)
     for request_num, request_page in enumerate(requests):
         if last_occurrences[request_page] <= request_num - 1:
-            is_starting_sol = last_occurrences[request_page] == 0 and request_page not in start_cache_state
             interval_problem.add_interval(Interval(start_time=last_occurrences[request_page], end_time=request_num - 1,
                                                    weight=page_weights[request_page], page=request_page,
-                                                   request=request_num, is_starting_sol=is_starting_sol))
+                                                   request=request_num))
         last_occurrences[request_page] = request_num + 1
 
     # add requests at the end
     for request_page in set(requests):
         if last_occurrences[request_page] <= time_horizon - 1:
-            is_starting_sol = last_occurrences[request_page] == 0 and request_page not in start_cache_state
             interval_problem.add_interval(Interval(start_time=last_occurrences[request_page], end_time=time_horizon - 1,
                                                    weight=page_weights[request_page], page=request_page,
-                                                   request="end", is_starting_sol=is_starting_sol))
+                                                   request="end"))
 
     return interval_problem
 
@@ -112,37 +109,42 @@ def translate_interval_solution_to_paging_solution(interval_prob: IntervalProble
     return cache_states
 
 
-def solve_local_ratio(interval_prob: IntervalProblem) -> List[Interval]:
-    plot_intervals_by_page(interval_prob.interval_list)
-
+def solve_local_ratio(interval_prob: IntervalProblem, recursion_depth: int) -> List[Interval]:
     new_interval_prob = copy.deepcopy(interval_prob)
     tau = new_interval_prob.find_tau()
+    plot_intervals_by_page(interval_prob.interval_list, save_file=f"[{recursion_depth}]tau{tau}.png")
     print(f"tau: {tau}")
-    if tau is None:
-        # Stopping condition
+
+    if len(interval_prob.find_weightless_intervals(tau)) >= interval_prob.width:
+        # STOPPING CONDITION
         return []
 
-    tau_intervals = new_interval_prob.find_non_starting_sol_intervals(tau)
-    min_interval = min(tau_intervals, key=lambda interval: interval.weight)
-    min_weight = min_interval.weight
+    tau_intervals = new_interval_prob.find_non_weightless_intervals(tau)
+    min_weight = min(tau_intervals, key=lambda interval: interval.weight).weight
     for interval in tau_intervals:
         interval.weight -= min_weight
 
-    min_interval.is_starting_sol = True
-    returned_sol = solve_local_ratio(new_interval_prob)
+    # find Q-intervals
+    q_intervals = [interval for interval in tau_intervals if interval.weight == 0]
+    returned_sol = solve_local_ratio(new_interval_prob, recursion_depth + 1)
 
-    # Check if returned_sol contains an interval of tau_interval
-    returned_sol_uuids = {interval.id for interval in returned_sol}
-    tau_intervals_uuids = {interval.id for interval in tau_intervals}
-    if len(returned_sol_uuids & tau_intervals_uuids) > 0:
-        # if so, don't add
-        return returned_sol
-    # Otherwise, add min_tau_interval
-    returned_sol.append(min_interval)
+    # Add q-intervals to solution as long as it is necessary
+    for interval in q_intervals:
+        # check if necessary:
+        is_necessary = False
+        for t in range(interval.start_time, interval.end_time + 1):
+            sol_t_intervals = [sol_interval for sol_interval in returned_sol if sol_interval.contains(t)]
+            if len(sol_t_intervals) + len(interval_prob.find_weightless_intervals(t)) < interval_prob.width:
+                is_necessary = True
+
+        if is_necessary:
+            print(f"Adding interval {interval}")
+            returned_sol.append(interval)
+
     return returned_sol
 
 
-def plot_intervals_by_page(intervals: List[Interval], highlight_ids=None):
+def plot_intervals_by_page(intervals: List[Interval], highlight_ids=None, save_file=None):
     """Plot a list of intervals, grouping them by the same page on the y-axis."""
     # Group intervals by page
     if highlight_ids is None:
@@ -168,8 +170,8 @@ def plot_intervals_by_page(intervals: List[Interval], highlight_ids=None):
 
             # Annotate the interval with its weight
             ax.text((interval.start_time + interval.end_time) / 2, y_level,
-                    f'Weight: {interval.weight:.2f}\nis_starting_sol: {interval.is_starting_sol}',
-                    va='center', ha='center', color='black')
+                    f'Weight: {interval.weight:.2f}\n',
+                    va='center', ha='center', color='black', fontsize=8)
 
         y_level += 1  # Move to the next level for the next page group
 
@@ -178,17 +180,21 @@ def plot_intervals_by_page(intervals: List[Interval], highlight_ids=None):
     ax.set_ylabel('Pages')
     ax.set_title('Intervals grouped by page as a function of time')
     ax.set_yticks([])  # Hide default y-ticks since we're manually labeling the pages
-
-    plt.show()
+    if save_file is not None:
+        plt.savefig(save_file)
+    else:
+        plt.show()
+    plt.clf()
+    plt.close()
 
 
 def main():
     requests = [4, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 4]
     page_weights = [3, 2, 3, 3, 3]
-    start_cache = [0, 4]
-    k = 2
+    start_cache = [0, 2, 3, 4]
+    k = 4
     interval_problem = translate_paging_to_interval_problem(requests, page_weights, k, start_cache)
-    sol = solve_local_ratio(interval_problem)
+    sol = solve_local_ratio(interval_problem, 0)
     print(translate_interval_solution_to_paging_solution(interval_problem, sol, requests))
     sol_uuids = {interval.id for interval in sol}
     plot_intervals_by_page(interval_problem.interval_list, sol_uuids)
